7951c73eba1112475ee7a0969ccd00a75216daee
timmarr98 <tmarr006@fiu.edu>
2021-07-04 23:51:28 -0400

    A lot of refactoring, will fix hangman

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,53 +1,100 @@
-import java.util.HashMap;
-import java.util.HashSet;
-
-public class Hangman {
-    private String mysteryWord;
-    private boolean onGoing;
-    private final int fails = 8;
-    HashMap<Integer, String> hangView = new HashMap<>();
-
-    public Hangman(String mysteryWord, Boolean onGoing)
-    {
-        this.mysteryWord = mysteryWord;
-        this.onGoing = onGoing;
-        hangView.put(0," ------\n" + "|     |\n" + "|\n" + "|\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------" );
-        hangView.put(1, " ------\n" + "|     |\n" + "|     0\n" + "|\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
-        hangView.put(2, " ------\n" + "|     |\n" + "|     0\n" + "|     +\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------\n");
-        hangView.put(3, " ------\n" + "|     |\n" + "|     0\n" + "|    -+\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
-        hangView.put(4," ------\n" + "|     |\n" + "|     0\n" + "|    -+-\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
-        hangView.put(5, " ------\n" + "|     |\n" + "|     0\n" + "|   /-+-\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
-        hangView.put(6, " ------\n" + "|     |\n" + "|     0\n" + "|   /-+-/\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
-        hangView.put(7, " ------\n" + "|     |\n" + "|     0\n" + "|   /-+-/\n" + "|     |\n" + "|\n" + "|\n" + "|\n" + "----------");
-    }
-
-//    private void initGame(String mysteryWord)
+//import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+//import net.dv8tion.jda.api.entities.TextChannel;
+//import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+//import net.dv8tion.jda.api.hooks.ListenerAdapter;
+//import org.w3c.dom.Text;
+//
+//import java.util.HashMap;
+//import java.util.HashSet;
+//
+//public class Hangman extends ListenerAdapter {
+//    private final GuildMessageReceivedEvent event;
+//    private String mysteryWord;
+//    private boolean onGoing;
+//    private final int fails = 8;
+//    private HashMap<Integer, String> hangView = new HashMap<>();
+//    private TextChannel tc;
+//    private String id;
+//    private EventWaiter waiter;
+//    private int i =0;
+//
+////    private HashSet<Character> bank = new HashSet<>();
+//    public Hangman(String mysteryWord, Boolean onGoing, GuildMessageReceivedEvent event, String id, EventWaiter waiter)
 //    {
+//        this.mysteryWord = mysteryWord;
+//        this.onGoing = onGoing;
+//        hangView.put(0," ------\n" + "|     |\n" + "|\n" + "|\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------" );
+//        hangView.put(1, " ------\n" + "|     |\n" + "|     0\n" + "|\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
+//        hangView.put(2, " ------\n" + "|     |\n" + "|     0\n" + "|     +\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------\n");
+//        hangView.put(3, " ------\n" + "|     |\n" + "|     0\n" + "|    -+\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
+//        hangView.put(4," ------\n" + "|     |\n" + "|     0\n" + "|    -+-\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
+//        hangView.put(5, " ------\n" + "|     |\n" + "|     0\n" + "|   /-+-\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
+//        hangView.put(6, " ------\n" + "|     |\n" + "|     0\n" + "|   /-+-/\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
+//        hangView.put(7, " ------\n" + "|     |\n" + "|     0\n" + "|   /-+-/\n" + "|     |\n" + "|\n" + "|\n" + "|\n" + "----------");
+//        this.event = event;
+//        this.tc = tc;
+//        tc = event.getChannel();
+//        this.id = id;
+//        this.waiter = waiter;
+//    }
+////    @Override
+////    public void onGuildMessageReceived(GuildMessageReceivedEvent e)
+////    {
+////        if (e.getChannel() == event.getGuild().getTextChannelsByName("hangman", true).get(0) && onGoing == true && )
+////        {
+////
+////        }
+//////        else
+//////        {
+//////
+//////        }
+////    }
+//
+//
+//    public String getMystertyWord()
+//    {
+//        return mysteryWord;
+//    }
+//
+//    public Boolean getonGoing()
+//    {
+//        return onGoing;
+//    }
+//
+//    public void playHangman()
+//    {
+//        TextChannel textChannel = event.getGuild().getTextChannelsByName("hangman",true).get(0);
+//        HashSet<Character> bank = new HashSet<>();
+//        int i =0; //The amount of fails
+////        tc.sendMessage(hangView.get(0)).queue();
+//
+//            textChannel.sendMessage("Welcome to Hangman \n " + hangView.get(i)).queue((message) ->
+//                    waiter.waitForEvent(GuildMessageReceivedEvent.class,
+//                            e->
+//                            {
+//                                if(e.getAuthor().getId().equals(id) || textChannel != e.getChannel() || e.getMessage().getContentRaw().length()>1)
+//                                    return false;
+//                                else
+//                                    return true;
+//                            },
+//                            e->
+//                            {
+//                                if(bank.contains(e.getMessage().getContentRaw().charAt(0)))
+//                                {
+//                                    textChannel.sendMessage("This letter has already been used").queue();
+//
+//                                }
+//                                if(!bank.contains(e.getMessage().getContentRaw()))
+//                                {
+//                                    bank.add(e.getMessage().getContentRaw().charAt(0));
+//                                }
+//
+//                                textChannel.sendMessage("NOT from partyleader").queue();
+//
+//                            }
+//            ));
+//
 //
 //    }
-
-    public String getMystertyWord()
-    {
-        return mysteryWord;
-    }
-
-    public Boolean getonGoing()
-    {
-        return onGoing;
-    }
-
-    public void playHangman()
-    {
-        onGoing = true;
-        HashSet<Character> mysteryChars = new HashSet<>();
-        for(int i=0; i< mysteryWord.length(); i++)
-        {
-            mysteryChars.add(mysteryWord.charAt(i));
-        }
-        int tries = 0;
-        while(tries < fails)
-        {
-
-        }
-    }
-}
+//
+//}
--- src/main/java/playHang.java
+++ src/main/java/playHang.java
@@ -1,36 +1,95 @@
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class playHang extends ListenerAdapter {
 
     private final EventWaiter waiter;
+    private int i;
+    private boolean game = false;
+    private HashMap<Integer, String> hangView = new HashMap<>();
+    private String mysteryWord;
+    private String gameOwner;
 
 
-    public playHang(EventWaiter waiter)
-    {
+    public playHang(EventWaiter waiter) {
         this.waiter = waiter;
+        game = false;
+        i = 0;
+        hangView.put(0," ------\n" + "|     |\n" + "|\n" + "|\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------" );
+        hangView.put(1, " ------\n" + "|     |\n" + "|     0\n" + "|\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
+        hangView.put(2, " ------\n" + "|     |\n" + "|     0\n" + "|     +\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------\n");
+        hangView.put(3, " ------\n" + "|     |\n" + "|     0\n" + "|    -+\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
+        hangView.put(4," ------\n" + "|     |\n" + "|     0\n" + "|    -+-\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
+        hangView.put(5, " ------\n" + "|     |\n" + "|     0\n" + "|   /-+-\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
+        hangView.put(6, " ------\n" + "|     |\n" + "|     0\n" + "|   /-+-/\n" + "|\n" + "|\n" + "|\n" + "|\n" + "----------");
+        hangView.put(7, " ------\n" + "|     |\n" + "|     0\n" + "|   /-+-/\n" + "|     |\n" + "|\n" + "|\n" + "|\n" + "----------");
     }
 
-   @Override
-    public void onGuildMessageReceived(GuildMessageReceivedEvent event)
-   {
-       if(event.getMessage().getContentRaw().equals("!hangman"))
-       {
-           TextChannel tc = event.getChannel();
-           String partyLeader = event.getAuthor().getId();
-           System.out.println(partyLeader);
-           event.getChannel().sendMessage("Hi, please DM me your word/phrase!").queue((message) ->
-                   waiter.waitForEvent(PrivateMessageReceivedEvent.class, e ->
-                   {
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        TextChannel textChannel = event.getGuild().getTextChannelsByName("hangman", true).get(0);
 
-                       return e.getAuthor().getId().equals(partyLeader);
-                   }, e -> tc.sendMessage(e.getMessage().getContentRaw()).queue(),7, TimeUnit.SECONDS,() -> event.getChannel().sendMessage("Took too long").queue()));
-       }
-   }
+        if (event.getMessage().getContentRaw().equals("!hangman")) {
+            if(game == true)
+            {
+                textChannel.sendMessage("There is currently a game in progress hosted by " + gameOwner);
+            }
+            else {
+                TextChannel tc = event.getChannel();
+                String partyLeader = event.getAuthor().getId();
+                System.out.println(partyLeader);
+                event.getChannel().sendMessage("Hi, please DM me your word/phrase!").queue((message) ->
+                        waiter.waitForEvent(PrivateMessageReceivedEvent.class, e ->
+                        {
+                            return e.getAuthor().getId().equals(partyLeader);
+                        }, e -> sender(event, e.getMessage().getContentRaw(), textChannel, partyLeader), 15, TimeUnit.SECONDS, () -> event.getChannel().sendMessage("Took too long").queue()));
+            }
+            }
 
+
+//        if(event.getMessage().getContentRaw().equals("e") && game == true && event.getChannel() == textChannel &&  !event.getAuthor().isBot())
+//        {
+//            textChannel.sendMessage("Welcome to Hangman! " + hangView.get(i) ).queue();
+//        }
+        if(event.getMessage().getContentRaw().equals(mysteryWord)&&  !event.getAuthor().isBot())
+        {
+            textChannel.sendMessage("You've correctly guessed the word " + mysteryWord + "!").queue();
+            reset();
+        }
+    }
+
+    public void sender(GuildMessageReceivedEvent event, String word, TextChannel t,String partyLeader) {
+        game = true;
+        t.sendMessage(word).queue();
+        mysteryWord = word;
+        gameOwner = partyLeader;
+
+    }
+
+    public void reset(){
+        game = false;
+        mysteryWord = "";
+        i =0;
+
+    }
+
+//    @Override
+//    public void onMessageReceived(MessageReceivedEvent e)
+//    {
+//        if(e.getMessage().getContentRaw().equals("e") && game == true )
+//        {
+//            System.out.println("2sdsd2dsd");
+//        }
+//    }
 }
+//   public void onMessage
+//
+//}
